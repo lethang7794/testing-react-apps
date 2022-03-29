0679d289b5da8ee0f293f8dc63eddba2fbd30f48
Thang Le <thanglq@inspirelab.io>
2022-03-29 20:12:19 +0700

    Ex 3: Avoid implementation details

--- src/__tests__/exercise/03.tsx
+++ src/__tests__/exercise/03.tsx
@@ -2,22 +2,19 @@
 // http://localhost:3000/counter
 
 // 🐨 add `screen` to the import here:
-import {render, fireEvent} from '@testing-library/react'
+import {render, fireEvent, screen} from '@testing-library/react'
 import Counter from '../../components/counter'
 
 test('counter increments and decrements when the buttons are clicked', () => {
   const {container} = render(<Counter />)
   // 🐨 replace these with screen queries
   // 💰 you can use `getByText` for each of these (`getByRole` can work for the button too)
-  const [decrement, increment] = Array.from(
-    container.querySelectorAll('button'),
-  )
+  // const [decrement, increment] = Array.from(
+  //   container.querySelectorAll('button'),
+  // )
+  const decrement = screen.getByText('Decrement')
+  const increment = screen.getByText('Increment')
 
-  // 🦺 you'll notice that React Testing Library queries do null-checks for you
-  // automatically, so you can ditch all this null-checking stuff.
-  if (!decrement || !increment) {
-    throw new Error('decrement and increment not found')
-  }
   if (!(container.firstChild instanceof HTMLElement)) {
     throw new Error('first child is not a div')
   }
