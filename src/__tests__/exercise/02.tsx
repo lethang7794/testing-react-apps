b2c9bf8628eccfa71fc5084a0405c42e03243f53
Thang Le <thanglq@inspirelab.io>
2022-03-29 19:53:51 +0700

    Ex 2: simple test with React Testing Library

--- src/__tests__/exercise/02.tsx
+++ src/__tests__/exercise/02.tsx
@@ -1,8 +1,9 @@
 // simple test with React Testing Library
 // http://localhost:3000/counter
 
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
 // 🐨 import the `render` and `fireEvent` utilities from '@testing-library/react'
+import {fireEvent, render} from '@testing-library/react'
 import Counter from '../../components/counter'
 
 // 💣 remove this. React Testing Library does this automatically!
@@ -11,29 +12,28 @@ beforeEach(() => {
 })
 
 test('counter increments and decrements when the buttons are clicked', () => {
-  // 💣 remove these two lines, React Testing Library will create the div for you
-  const div = document.createElement('div')
-  document.body.append(div)
-
   // 🐨 swap ReactDOM.render with React Testing Library's render
   // Note that React Testing Library's render doesn't need you to pass a `div`
   // so you only need to pass one argument. render returns an object with a
   // bunch of utilities on it. For now, let's just grab `container` which is
   // the div that React Testing Library creates for us.
   // 💰 const {container} = render(<Counter />)
-  ReactDOM.render(<Counter />, div)
+  // ReactDOM.render(<Counter />, div)
+  const {container} = render(<Counter />)
 
   // 🐨 instead of `div` here you'll want to use the `container` you get back
   // from React Testing Library
-  const [decrement, increment] = Array.from(div.querySelectorAll('button'))
+  const [decrement, increment] = Array.from(
+    container.querySelectorAll('button'),
+  )
   if (!decrement || !increment) {
     throw new Error('decrement and increment not found')
   }
-  if (!(div.firstChild instanceof HTMLElement)) {
+  if (!(container.firstChild instanceof HTMLElement)) {
     throw new Error('first child is not a div')
   }
 
-  const message = div.firstChild.querySelector('div')
+  const message = container.firstChild.querySelector('div')
   if (!message) {
     throw new Error(`couldn't find message div`)
   }
@@ -41,18 +41,9 @@ test('counter increments and decrements when the buttons are clicked', () => {
   expect(message.textContent).toBe('Current count: 0')
 
   // 🐨 replace the next two statements with `fireEvent.click(button)`
-  const incrementClickEvent = new MouseEvent('click', {
-    bubbles: true,
-    cancelable: true,
-    button: 0,
-  })
-  increment.dispatchEvent(incrementClickEvent)
+  fireEvent.click(increment)
   expect(message.textContent).toBe('Current count: 1')
-  const decrementClickEvent = new MouseEvent('click', {
-    bubbles: true,
-    cancelable: true,
-    button: 0,
-  })
-  decrement.dispatchEvent(decrementClickEvent)
+
+  fireEvent.click(decrement)
   expect(message.textContent).toBe('Current count: 0')
 })
