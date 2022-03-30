f89c05692910546e919f0705932c390a8b55a12d
Thang Le <thanglq@inspirelab.io>
2022-03-30 20:59:41 +0700

    Ex 4 - Extra 4: use Test Data Bot

--- src/__tests__/exercise/04.tsx
+++ src/__tests__/exercise/04.tsx
@@ -3,26 +3,23 @@
 
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import faker from 'faker'
 import Login from '../../components/login'
 import type {LoginFormValues} from '../../components/login'
+import {build, fake} from '@jackfranklin/test-data-bot'
 
-const buildLoginForm = (overrides?: Partial<LoginFormValues>) => {
-  return {
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...overrides,
-  }
-}
+const buildLoginForm = build<LoginFormValues>({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password()),
+  },
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
 
   render(<Login onSubmit={handleSubmit} />)
 
-  const {username, password} = buildLoginForm({
-    username: 'bestUsernameInTheWord',
-  })
+  const {username, password} = buildLoginForm()
 
   const usernameField = screen.getByLabelText('Username')
   userEvent.type(usernameField, username)
