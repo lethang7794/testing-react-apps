cdf928d327bf6d1fbd1f81773e80a785583ac721
Thang Le <thanglq@inspirelab.io>
2022-04-02 16:18:43 +0700

    Ex 8 - Extra 1: fake component

--- src/__tests__/exercise/08.tsx
+++ src/__tests__/exercise/08.tsx
@@ -1,35 +1,25 @@
 // testing custom hooks
 // http://localhost:3000/counter-hook
 
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-
-function CounterHookTest() {
-  const {count, increment, decrement} = useCounter()
-  return (
-    <div>
-      <div>Current count: {count}</div>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-    </div>
-  )
-}
-
 test('exposes the count and increment/decrement functions', () => {
-  render(<CounterHookTest />)
-  const count = screen.getByText(/current count/i)
-  const increment = screen.getByRole('button', {name: /increment/i})
-  const decrement = screen.getByRole('button', {name: /decrement/i})
+  let result: ReturnType<typeof useCounter>
+  function TestCounterHook(props) {
+    result = useCounter(props)
+    return null
+  }
 
-  expect(count).toHaveTextContent('Current count: 0')
+  render(<TestCounterHook />)
 
-  userEvent.click(increment)
-  expect(count).toHaveTextContent('Current count: 1')
+  expect(result.count).toBe(0)
 
-  userEvent.click(decrement)
-  expect(count).toHaveTextContent('Current count: 0')
+  act(() => result.increment())
+  expect(result.count).toBe(1)
+
+  act(() => result.decrement())
+  expect(result.count).toBe(0)
 })
 
 /*
