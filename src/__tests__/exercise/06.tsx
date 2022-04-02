0ec2fa33ff16b3f874b3a73523d4098c16b5d6f2
Thang Le <thanglq@inspirelab.io>
2022-04-02 13:46:23 +0700

    Ex 6 - Extra 2: prep

--- src/__tests__/exercise/06.tsx
+++ src/__tests__/exercise/06.tsx
@@ -4,27 +4,6 @@
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import Location from '../../examples/location'
 
-const mockedGeolocation = {
-  getCurrentPosition: jest.fn(),
-}
-
-beforeAll(() => {
-  Object.defineProperty(window.navigator, 'geolocation', {
-    value: mockedGeolocation,
-  })
-})
-
-function deferred() {
-  let resolve: (value?: unknown) => void
-  let reject: (reason?: unknown) => void
-  const promise = new Promise((res, rej) => {
-    resolve = res
-    reject = rej
-  })
-
-  return {promise, resolve, reject}
-}
-
 test('displays the users current location', async () => {
   // 📜 https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPosition
   const fakePosition = {
@@ -33,55 +12,9 @@ test('displays the users current location', async () => {
       longitude: '105',
     },
   }
-
-  const {promise, resolve} = deferred()
-
-  mockedGeolocation.getCurrentPosition.mockImplementation(
-    (successCallback, errorCallback) => {
-      promise.then(() => {
-        successCallback(fakePosition)
-      })
-    },
-  )
-
-  render(<Location />)
-
-  expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
-
-  resolve()
-
-  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
-
-  expect(screen.getByText(/latitude/i)).toHaveTextContent(
-    `Latitude: ${fakePosition.coords.latitude}`,
-  )
-  expect(screen.getByText(/longitude/i)).toHaveTextContent(
-    `Longitude: ${fakePosition.coords.longitude}`,
-  )
 })
 
-test('displays error message if cannot get current position', async () => {
-  const {promise, resolve} = deferred()
-
-  const errorMessage = 'Failed to get current position'
-  mockedGeolocation.getCurrentPosition.mockImplementation(
-    (successCallback, errorCallback) => {
-      promise.then(() => {
-        errorCallback(new Error(errorMessage))
-      })
-    },
-  )
-
-  render(<Location />)
-
-  expect(screen.getByLabelText(/loading/i)).toBeInTheDocument()
-
-  resolve()
-
-  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
-
-  expect(screen.getByRole('alert')).toHaveTextContent(errorMessage)
-})
+test('displays error message if cannot get current position', async () => {})
 
 /*
 eslint
